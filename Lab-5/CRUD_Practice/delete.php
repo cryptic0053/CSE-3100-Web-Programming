fdec1fdd6dc10eda88829b84743f0ddf285b9e10
Anirban Argha <112749165+cryptic0053@users.noreply.github.com>
2024-02-20 13:30:08 +0600

    delete.php

--- Lab-5/CRUD_Practice/delete.php
+++ Lab-5/CRUD_Practice/delete.php
@@ -1,4 +1,31 @@
 <?php
 
-// Write your code to delete data
+$connection = mysqli_connect('localhost', 'root', '', 'reg_db_b2');
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+if(isset($_GET['Id'])) {
+    
+    $del_id = mysqli_real_escape_string($connection, $_GET['Id']);
+
    
+    $delete_query = "DELETE FROM reg_table WHERE Id = $del_id";
+
+    $delete = mysqli_query($connection, $delete_query);
+
+    if ($delete) {
+        
+        header("location: view.php");
+        exit(); 
+    } else {
+        echo "Error deleting record: " . mysqli_error($connection);
+    }
+} else {
+    echo "No ID parameter provided";
+}
+
+mysqli_close($connection);
+?>
