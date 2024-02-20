475339fefa92fddeab076434f9ad03424fa7bbd5
Anirban Argha <112749165+cryptic0053@users.noreply.github.com>
2024-02-20 13:33:05 +0600

    view.php

--- Lab-5/CRUD_Practice/view.php
+++ Lab-5/CRUD_Practice/view.php
@@ -11,11 +11,35 @@
 <body>
 
     <?php
+    $connection = mysqli_connect('localhost', 'root', '', 'reg_db_b2');
 
-    // Write your code to retrieve data
-     
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $show_query = "SELECT * FROM reg_table";
+    $show_result = mysqli_query($connection, $show_query);
+
+    if (mysqli_num_rows($show_result) > 0) {
+        echo "<table>";
+        echo "<tr><th>Id</th><th>Username</th><th>Email</th><th>Password</th><th>Action</th></tr>";
+        while ($row = mysqli_fetch_assoc($show_result)) {
+            echo "<tr>";
+            echo "<td>" . $row["Id"] . "</td>";
+            echo "<td>" . $row["Username"] . "</td>";
+            echo "<td>" . $row["Email"] . "</td>";
+            echo "<td>" . $row["Password"] . "</td>";
+            echo "<td><a href='delete.php?id=" . $row["Id"] . "'>Delete</a></td>";
+            echo "<td><a href='update.php?id=". $row["Id"] . "'>Update</a></td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No data in database";
+    }
+
+    mysqli_close($connection);
     ?>
-
 
 </body>
 
