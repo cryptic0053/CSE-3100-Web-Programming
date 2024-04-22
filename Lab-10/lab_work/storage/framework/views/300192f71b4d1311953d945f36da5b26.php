2e9d3d6c3f7be07f1c319f8a62409819817612d2
Anirban Argha <112749165+cryptic0053@users.noreply.github.com>
2024-04-22 12:04:58 +0600

    Add files via upload

---
+++ Lab-10/lab_work/storage/framework/views/300192f71b4d1311953d945f36da5b26.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\study\3-1\2k20 Materials\Lab\CSE 3100 - Web Programming\lab-10\lab_work\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
